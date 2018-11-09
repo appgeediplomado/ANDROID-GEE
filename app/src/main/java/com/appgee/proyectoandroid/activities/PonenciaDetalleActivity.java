450315a9b1332264ad4dd8cbd418fe712160712f
romansg <romans@unam.mx>
2018-11-09 07:43:30 -0600

    Terminados los detalles de la ponencia

--- app/src/main/java/com/appgee/proyectoandroid/activities/PonenciaDetalleActivity.java
+++ app/src/main/java/com/appgee/proyectoandroid/activities/PonenciaDetalleActivity.java
@@ -1,9 +1,12 @@
 package com.appgee.proyectoandroid.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.appgee.proyectoandroid.R;
@@ -11,7 +14,13 @@ import com.appgee.proyectoandroid.models.Ponencia;
 
 public class PonenciaDetalleActivity extends AppCompatActivity {
     Toolbar toolbar;
-    TextView tvPonenciaTitulo;
+    TextView tvTituloPonencia;
+    TextView tvNombrePonente;
+    TextView tvFechaPonencia;
+    TextView tvHoraPonencia;
+    TextView tvLugarPonencia;
+    Button btnCalificarPonencia;
+
     Ponencia ponencia = null;
 
     @Override
@@ -24,12 +33,30 @@ public class PonenciaDetalleActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);  //Pone el menu hamburguesa
         getSupportActionBar().setTitle("Actividad");
 
-        tvPonenciaTitulo = findViewById(R.id.tvDetallesPonenciaTitulo);
+        tvTituloPonencia = findViewById(R.id.tvTituloPonencia);
+        tvNombrePonente = findViewById(R.id.tvNombrePonente);
+        tvFechaPonencia = findViewById(R.id.tvFechaPonencia);
+        tvHoraPonencia = findViewById(R.id.tvHoraPonencia);
+        tvLugarPonencia = findViewById(R.id.tvLugarPonencia);
+        btnCalificarPonencia = findViewById(R.id.btnCalificarPonencia);
 
         ponencia = (Ponencia) getIntent().getSerializableExtra("ponencia");
 
         if (ponencia != null) {
-            tvPonenciaTitulo.setText(ponencia.getTitulo());
+            tvTituloPonencia.setText(ponencia.getTitulo());
+            tvNombrePonente.setText(ponencia.getNombrePonente());
+            tvFechaPonencia.setText(ponencia.getFecha());
+            tvHoraPonencia.setText(ponencia.getHora());
+            tvLugarPonencia.setText(ponencia.getLugar());
+
+            btnCalificarPonencia.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(PonenciaDetalleActivity.this, PonenciaEvaluacionActivity.class);
+                    intent.putExtra("ponencia", ponencia);
+                    startActivity(intent);
+                }
+            });
         }
     }
 
